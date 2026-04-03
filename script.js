cc67f383ff3583fab4a6c44727a8c645912f856b
carlhrv <habervocarlteody@gmail.com>
2026-04-03 19:15:13 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -11,34 +11,50 @@ const lastNameHint = document.getElementById("last-name-hint");
 const emailHint = document.getElementById("email-hint");
 const messageHint = document.getElementById("message-hint");
 
-function validate(input, helper) {
-    const isValid = input.value.trim() !== "";
-    helper.innerHTML = isValid ? "" : "*required";
-    return isValid;
+function validate(input, hint) {
+  const value = input.value.trim();
+  
+  if (value === "") {
+    hint.innerHTML = "*required";
+    return false;
+  }
+
+  // email validation
+  if (input.id === "email") {
+    const pattern = /\S+@\S+\.\S+/;
+    if (!pattern.test(value)) {
+      hint.innerHTML = "*invalid email";
+      return false;
+    }
+  }
+
+  hint.innerHTML = "";
+  return true;
 }
 
 function handleSubmit(event) {
-    event.preventDefault();
-    const isFirstNameValid = validate(firstName, firstNameHint);
-    const isLastNameValid = validate(lastName, lastNameHint);
-    const isEmailValid = validate(email, emailHint);
-    const isMessageValid = validate(message, messageHint);
+  event.preventDefault();
+  const isFirstNameValid = validate(firstName, firstNameHint);
+  const isLastNameValid = validate(lastName, lastNameHint);
+  const isEmailValid = validate(email, emailHint);
+  const isMessageValid = validate(message, messageHint);
 
-    if (isFirstNameValid && isLastNameValid && isEmailValid && isMessageValid) {
-        const templateParams = {
-            first_name: firstName.value,
-            last_name: lastName.value,
-            email: email.value,
-            message: message.value,
-        };
+  if (isFirstNameValid && isLastNameValid && isEmailValid && isMessageValid) {
+    const templateParams = {
+      first_name: firstName.value,
+      last_name: lastName.value,
+      email: email.value,
+      message: message.value,
+    };
 
-        emailjs.send('service_avta2i8', 'template_ctzzhc5', templateParams)
-            .then(() => {
-                alert("Sent!");
-                form.reset();
-            })
-            .catch(() => alert("Error sending email."));
-    }
+    emailjs
+      .send("service_avta2i8", "template_ctzzhc5", templateParams)
+      .then(() => {
+        alert("Sent!");
+        form.reset();
+      })
+      .catch(() => alert("Error sending email."));
+  }
 }
 
 form.addEventListener("submit", handleSubmit);
